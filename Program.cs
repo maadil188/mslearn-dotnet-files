07ddfd4e67f0cfc54fcaa36037362ffedd827cba
Mohammed Abdul Adil <maadil1970@gmail.com>
2026-07-04 23:31:00 +0530

    In this module, I learned how to read files and directories, create files and directories, and write data to files. Your code is going to run in fictional production at the largest fake company on the internet!
    A few things to keep in mind from this module:
    
    Directory.EnumerateDirectories and Directory.EnumerateFiles accept a parameter that allows you to specify a search condition for names to return, and a parameter to recursively traverse all child directories.
    System.Environment.SpecialFolder is an enumeration that lets you access system-defined folders, such as the desktop or user profile, in a cross-platform manner without having to memorize the exact path for each operating system.
    If you need to parse other file types, check out the packages on nuget.org. For example, you can use the CsvHelper package for .csv files.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
-﻿var salesFiles = FindFiles("stores");
-    
-foreach (var file in salesFiles)
-{
-    Console.WriteLine(file);
-}
+﻿using Newtonsoft.Json; 
+
+var currentDirectory = Directory.GetCurrentDirectory();
+var storesDirectory = Path.Combine(currentDirectory, "stores");
+
+var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+Directory.CreateDirectory(salesTotalDir);   
+
+var salesFiles = FindFiles(storesDirectory);
+
+var salesTotal = CalculateSalesTotal(salesFiles);
+
+File.AppendAllText(Path.Combine(salesTotalDir, "totals.txt"), $"{salesTotal}{Environment.NewLine}");
 
 IEnumerable<string> FindFiles(string folderName)
 {
@@ -13,8 +20,8 @@ IEnumerable<string> FindFiles(string folderName)
 
     foreach (var file in foundFiles)
     {
-        // The file name will contain the full path, so only check the end of it
-        if (file.EndsWith("sales.json"))
+        var extension = Path.GetExtension(file);
+        if (extension == ".json")
         {
             salesFiles.Add(file);
         }
@@ -22,3 +29,25 @@ IEnumerable<string> FindFiles(string folderName)
 
     return salesFiles;
 }
+
+double CalculateSalesTotal(IEnumerable<string> salesFiles)
+{
+    double salesTotal = 0;
+    
+    // Loop over each file path in salesFiles
+    foreach (var file in salesFiles)
+    {      
+        // Read the contents of the file
+        string salesJson = File.ReadAllText(file);
+    
+        // Parse the contents as JSON
+        SalesData? data = JsonConvert.DeserializeObject<SalesData?>(salesJson);
+    
+        // Add the amount found in the Total field to the salesTotal variable
+        salesTotal += data?.Total ?? 0;
+    }
+    
+    return salesTotal;
+}
+
+record SalesData (double Total);
